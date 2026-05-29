480621450f16292c2e291edf51f4a8c350570c89
parthgala1 <parth.gala1356@gmail.com>
2026-05-29 21:38:49 +0530

    found issue

--- server/main.py
+++ server/main.py
@@ -73,7 +73,11 @@ app.state.db_initialized = False
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://13.202.28.196:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
